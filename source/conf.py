4f6d9a9fdb62ca1335607fcca3e10eee62d47f9e
Youngeun Hwang <143184671+y0ungeunii@users.noreply.github.com>
2025-04-02 16:41:33 -0400

    Update general configuration

--- source/conf.py
+++ source/conf.py
@@ -11,18 +11,61 @@ copyright = '2025, Youngeun Hwang'
 author = 'Youngeun Hwang'
 release = '1.0 beta'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# -- General configuration
 
-extensions = []
+extensions = [
+    'nbsphinx',
+    'sphinx_gallery.load_style',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosectionlabel',
+    'sphinx_gallery.gen_gallery',
+    'myst_parser'
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'markdown',
+    '.md': 'markdown',
+}
+
+# source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+
+sphinx_gallery_conf = {
+    'examples_dirs': '../../tutorials',
+    # 'plot_gallery': 'False',
+    'thumbnail_size': (250, 250),
+    # 'image_scrapers': ('matplotlib', _get_sg_image_scraper()),
+    # 'within_subsection_order': FileNameSortKey,
+    'download_all_examples': False,
+    'remove_config_comments': True,
+    # 'run_stale_examples': True,
+}
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+#generate autosummary even if no references
+autosummary_generate = True
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = []
 
+# -- Options for HTML output
 
+html_theme = 'sphinx_rtd_theme'
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+html_show_sourcelink = False
+html_copy_source = False
